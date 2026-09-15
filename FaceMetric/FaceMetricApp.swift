3cabc5cc33b8cb78b78ece999c051672cb81fc34
Yundo Kim <yundokim0405@naver.com>
2026-09-16 06:17:05 +0900

    Initial Commit

---
+++ FaceMetric/FaceMetricApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceMetricApp.swift
+//  FaceMetric
+//
+//  Created by Yundo Kim on 9/16/26.
+//
+
+import SwiftUI
+
+@main
+struct FaceMetricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
